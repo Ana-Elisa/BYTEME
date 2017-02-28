805c4ad7caac99dfc8ff0dd600c7c569cfb64138
NeckbeardedDragon <kirbyfan95@gmail.com>
2017-02-28 13:24:36 -0500

    Started on the client side
    - Changed the project to a 2D configuration
    - Added a Player object
    - Added a Block prefab
    ? It moves slightly and only when you apply a very high force. Probably
    need to manually change velocity.

---
+++ Client/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float maxhspeed = 2f;
+	public float groundf = 1f;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			rb.drag
+		}
+		if (Input.GetAxis ("Horizontal") > 0f) {
+			if (rb.velocity.x < maxhspeed) {
+				rb.AddForce (Vector2.right * groundf);
+				print ("R");
+			}
+		}
+		if (Input.GetAxis ("Horizontal") < 0f) {
+			if (rb.velocity.x > -maxhspeed) {
+				rb.AddForce (Vector2.left * groundf);
+				print ("L");
+			}
+		}
+	}
+}
