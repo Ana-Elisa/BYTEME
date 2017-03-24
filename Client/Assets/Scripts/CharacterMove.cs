be36085b0a163ec50f848d0d1b56b5e4efd94642
kl7programming <kyle7liu8@gmail.com>
2017-03-24 18:53:46 -0400

    Added Cat and Created New Stage
    Created Scene L1.
    Cat Character in place. Stage in place.
    Camera not following, not animating and does not jump.

---
+++ Client/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	float speed = 2.0f;
+
+	// Update is called once per frame
+	void Update () {
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
