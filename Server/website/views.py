a4e4a1b6f8f3b68d643f62be825aa6d10a78fdca
MichaelCDormann <Michael.C.Dormann@gmail.com>
2017-03-25 12:53:19 -0400

    Fixed some graph things

--- Server/website/views.py
+++ Server/website/views.py
@@ -15,27 +15,39 @@ class AvgTimePerLevelBar(Chart):
         level0_data = GameSave.objects.filter(next_level=1)
         level1_data = GameSave.objects.filter(next_level=2)
         level2_data = GameSave.objects.filter(next_level=3)
-        level3_data = GameSave.objects.filter(next_level=4)
+        level3_data = GameSave.objects.filter(next_level__gt=3)
 
-        level0_time = 0
-        for level_data in level0_data:
-            level0_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
-        data.append(level0_time/len(level0_data))
+        if len(level0_data) > 0:
+            level0_time = 0
+            for level_data in level0_data:
+                level0_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
+            data.append(level0_time/len(level0_data))
+        else:
+            data.append(0)
 
-        level1_time = 0
-        for level_data in level1_data:
-            level1_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
-        data.append(level1_time/len(level1_data))
+        if len(level1_data) > 0:
+            level1_time = 0
+            for level_data in level1_data:
+                level1_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
+            data.append(level1_time/len(level1_data))
+        else:
+            data.append(0)
 
-        level2_time = 0
-        for level_data in level2_data:
-            level2_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
-        data.append(level2_time/len(level2_data))
+        if len(level2_data) > 0:
+            level2_time = 0
+            for level_data in level2_data:
+                level2_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
+            data.append(level2_time/len(level2_data))
+        else:
+            data.append(0)
 
-        level3_time = 0
-        for level_data in level3_data:
-            level3_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
-        data.append(level3_time/len(level3_data))
+        if len(level3_data) > 0:
+            level3_time = 0
+            for level_data in level3_data:
+                level3_time += (level_data.time.days * 25 + level_data.time.seconds/3600)
+            data.append(level3_time/len(level3_data))
+        else:
+            data.append(0)
 
         colors = [
             rgba(255, 99, 132, 0.2),
