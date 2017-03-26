25430a4f7d0f8653a85cbb728b93e9458397f97d
kl7programming <kyle7liu8@gmail.com>
2017-03-26 18:34:54 -0400

    Camera Follows Character and Character Turns to direction of movement

---
+++ Client/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	GameObject target;
+	Vector3 offset;
+
+	void Start()
+	{
+		target = GameObject.Find ("cat");
+		offset = transform.position - target.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void LateUpdate()
+	{
+		transform.position = target.transform.position + offset;
+	}
+}
--- Client/Assets/Scripts/CharacterMove.cs
+++ Client/Assets/Scripts/CharacterMove.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
-	float speed = 2.0f;
+	float speed = .05f;
+	float jump = 15f;
+	bool grounded;
+	Vector3 lastDir;
+
+	Animator anim;
+
+	GameObject character;
+
+	void Start()
+	{
+		lastDir = new Vector3 (1, 0, 0);
+		character = GameObject.Find ("cat");
+		anim = GetComponent<Animator> ();
+	}
 
 	// Update is called once per frame
-	void Update () {
-		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
-		transform.position += move * speed * Time.deltaTime;
+	void Update () 
+	{
+		anim.SetInteger ("Status", 0);
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		var move = new Vector3 (horizontal, 0, 0);
+	
+		if (horizontal != 0) 
+		{
+			if (anim.GetInteger ("Status") != 1) 
+			{
+				anim.SetInteger ("Status", 1);	
+			}
+			lastDir = move;
+		}
+
+		transform.rotation = Quaternion.LookRotation (lastDir);	
+
+		character.transform.position += move * speed;
 	}
+
 }
