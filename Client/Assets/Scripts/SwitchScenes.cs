0d0a72f20ba8b9deb7ce256a9f71f50e80dc12e0
meganmolumby <megan.gandy@hotmail.com>
2017-03-29 22:48:37 -0400

    setup inputfields so input can be retrieved from LoginScreen and ForgotPasswordScreen

--- Client/Assets/Scripts/SwitchScenes.cs
+++ Client/Assets/Scripts/SwitchScenes.cs
@@ -11,11 +11,19 @@ public class SwitchScenes : MonoBehaviour {
     private Button enterGameButton;
     private Button forgotPasswordButton;
     private Button newUserButton;
+    private Button submitButton;
+    private InputField usernameInputField;
+    private InputField passwordInputField;
+    private InputField emailInputField;
+    private String username;
+    private String password;
+    private String email;
 
 	// Use this for initialization
 	void Start () {
         UnityEngine.Object.DontDestroyOnLoad(this);
-	}
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -45,7 +53,18 @@ public class SwitchScenes : MonoBehaviour {
             enterGameButton = GameObject.Find("EnterGameButton").GetComponent<Button>();
             forgotPasswordButton = GameObject.Find("ForgotPasswordButton").GetComponent<Button>();
             newUserButton = GameObject.Find("NewUserButton").GetComponent<Button>();
+            //setup input fields
+            usernameInputField = GameObject.Find("UsernameInputField").GetComponent<InputField>();
+            passwordInputField = GameObject.Find("PasswordInputField").GetComponent<InputField>();
+            //after user is done editing input, 
+            usernameInputField.onEndEdit.AddListener(delegate { UpdateUserName(usernameInputField.text); });
+            passwordInputField.onEndEdit.AddListener(delegate { UpdatePassword(passwordInputField.text); });
+            
+
+
+
             //if enter game button clicked go to game scene
+            
             enterGameButton.onClick.AddListener(loadGame);
             //forgot password button clicked -> forgotpasswordScreen
             forgotPasswordButton.onClick.AddListener(loadForgotPasswordScreen);
@@ -54,26 +73,52 @@ public class SwitchScenes : MonoBehaviour {
 
 
         }
-      /*  else if (scene.name == "ForgotPasswordScreen")
+       else if (scene.name == "ForgotPasswordScreen")
         {
+            //setup button
+            submitButton = GameObject.Find("SubmitButton").GetComponent<Button>();
+            emailInputField = GameObject.Find("EmailInputField").GetComponent<InputField>();
+            emailInputField.onEndEdit.AddListener(delegate { UpdateEmail(emailInputField.text); });
+            //if submit clicked return to login screen. 
+            //This is also where a post method will be sent to server
+            submitButton.onClick.AddListener(loadLoginScreen);
 
 
-        }
+        }/*
         else if (scene.name == "NewUserScreen") {
 
         }
         */
     }
+    private void UpdateEmail(string arg0) {
+        email = arg0;
+        Debug.Log(arg0);
+    }
+    private void UpdatePassword(string arg0) {
+        password = arg0;
+        Debug.Log(arg0);
+    }
+    private void UpdateUserName(string arg0) {
+        username = arg0;
+        Debug.Log(arg0);
+    }
 
     private void loadGame()
     {
+        //Here is where we would validate username and password, GEt/Post 
+        //method to service 
         SceneManager.LoadScene("Test");
+      
+       
     }
     private void loadForgotPasswordScreen() {
         SceneManager.LoadScene("ForgotPasswordScreen");
     }
     private void loadNewUserScreen() {
         SceneManager.LoadScene("NewUserScreen");
+    }
+    private void loadLoginScreen() {
+        SceneManager.LoadScene("LoginScreen");
     }
 
     
