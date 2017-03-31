c6cb38d6a402bb79632d8d5f5b5a68b48a280683
ana <anaelisagarciavelez@gmail.com>
2017-03-31 18:30:01 -0400

    Started the player Health HUD Canvas

---
+++ Client/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public SliderJoint2D healthSlider;
+	bool isDead;
+	bool damaged;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'BYTEME_Server',
-        'USER': 'byteme',
-        'PASSWORD': 'byte1234ME!',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'BYTEME_Server',
+#        'USER': 'byteme',
+#        'PASSWORD': 'byte1234ME!',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
