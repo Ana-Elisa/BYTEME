cef6bdc9f351e1988ba5cf5362f1dbe1e04d4ed7
MichaelCDormann <Michael.C.Dormann@gmail.com>
2017-04-02 17:23:33 -0400

    Finished password reset. Fixed Kitty Krawler boarder

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -91,25 +91,30 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'BYTEME_Server',
-#        'USER': 'byteme',
-#        'PASSWORD': 'byte1234ME!',
-#        'HOST': 'localhost',
-#        'PORT': '',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BYTEME_Server',
+        'USER': 'byteme',
+        'PASSWORD': 'byte1234ME!',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'studiobyteme@gmail.com'
+EMAIL_HOST_PASSWORD = 'icecream123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
