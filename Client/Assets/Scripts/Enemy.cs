fac85599eef05278949e10583a16325794dee200
meganmolumby <megan.gandy@hotmail.com>
2017-04-10 11:34:35 -0400

    working on enemey knockback

--- Client/Assets/Scripts/Enemy.cs
+++ Client/Assets/Scripts/Enemy.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 	public int currentHealth;
 	public int maxHealth = 30;
+    public Rigidbody2D rgb2d;
+    public float knockback;
+    public float knockbackLength;//how long enemy knocked back for
+    public float knockbackCount;
+    public bool knockFromRight;
+
 	void Start(){
 		currentHealth = maxHealth;
-	}
-
+        rgb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (currentHealth <= 0) {
+            Destroy(gameObject);
+        }
+        //reaction to hit
+        if (knockbackCount <= 0)
+        {
+            //move enemy around
+        }
+        else {
+            if (knockFromRight) {
+                rgb2d.velocity = new Vector2(-knockback, knockback);
+            }
+            if (!knockFromRight) {
+                rgb2d.velocity = new Vector2(knockback, knockback);
+            }
+            knockbackCount -= Time.deltaTime;
+        }
+    }
+   
 	public void Damage(int damage){
 		currentHealth -= damage;
 		//create dying animation!
-		//GameObject.GetComponent<Animation>().Play("Player RedFlash");
+		
 	
 	}
 }
