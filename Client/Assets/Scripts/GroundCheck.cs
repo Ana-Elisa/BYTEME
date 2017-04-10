b3b72f29ecbb17a81ee8997eae3d360a622950d8
meganmolumby <megan.gandy@hotmail.com>
2017-04-10 09:57:03 -0400

    player walking with animation!!!

---
+++ Client/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    private PlayerC player;
+
+     void Start()
+    {
+        player = gameObject.GetComponentInParent<PlayerC>();
+    }
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        player.grounded = true;
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        player.grounded = false;
+    }
+}
---
+++ Client/Assets/Scripts/PlayerC.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerC : MonoBehaviour {
+	public float speed = 50f;
+    public float maxSpeed = 3; 
+	public float jumpPower = 150f;
+	public bool grounded;
+	private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+		
+	}
+	void FixedUpdate(){
+		float h = Input.GetAxis("Horizontal");
+       //moves player L R
+        rb2d.AddForce((Vector2.right * speed) * h);
+        //Limits speed of player
+        if (rb2d.velocity.x > maxSpeed) {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed) {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+		
+	}
+}
