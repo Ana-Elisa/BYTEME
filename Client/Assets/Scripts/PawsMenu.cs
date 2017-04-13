364acbc5ba4909e9c03ae8fe0e6193d99a632752
Alkadi, Danah <n00964531@unf.edu>
2017-04-12 21:51:14 -0400

    Partially working pause menu -Includes compiler errors

---
+++ Client/Assets/Scripts/PawsMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawsMenu : MonoBehaviour 
+{
+
+	bool paused = false;
+	public Canvas pawsMenu;
+
+	void Start()
+	{
+		pawsMenu.enabled = false; 
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Time.timeScale = 0;
+			print ("paused");
+			paused = true;
+			if (paused) 
+			{
+				pawsMenu.enabled = true;
+			} 
+			else if (!paused) 
+			{
+				pawsMenu.enabled = false;
+			}
+		}
+	}
+
+	void onClick()
+	{
+		//print ("continue");
+		//paused = false;
+		//Time.timeScale = 1;
+	}
+}
