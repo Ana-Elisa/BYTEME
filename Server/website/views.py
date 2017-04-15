845680a41f2b73b1dd23deacf1d8cc07d4c536ea
MichaelCDormann <Michael.C.Dormann@gmail.com>
2017-04-15 13:27:09 -0400

    Working leaderboard

--- Server/website/views.py
+++ Server/website/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .charts import *
+from KittyKrawler.models import GameSave
 
 
 def index(request):
     return render(request, "website/index.html")
 
 def charts(request):
+    save_queryset = GameSave.objects.all()
 
+    save_unpack = [(save.user.username, len(save.save_items.all()), save.attack, save.defence, save.speed,
+                    save.health, save.total_health, save.next_level, save.time)
+                   for save in save_queryset]
+
+    save_sorted = sorted(save_unpack,
+                         key=lambda s: ((s[1] + s[2] + s[3] + s[4] + s[5] + s[6]) * s[7]) - (s[8].total_seconds()/1000),
+                         reverse=True)
+
+    leaderboard = save_sorted[0:11]
 
     return render(request, "website/charts.html", {
         'bar_chart': AvgTimePerLevelBar(),
         'line_chart': TimeSpentvsLevel(),
         'pie_chart': PieChartItemsFound(),
+        'leaderboard': leaderboard
     })
