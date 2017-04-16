1a176eeec71f942148ddee9576b2eccf2c2f3ae3
NeckbeardedDragon <kirbyfan95@gmail.com>
2017-04-16 19:24:26 -0400

    Finished Floor generator
    More rooms can be added as desired, floors can be expanded without
    issue.
    
    Also made a camera script that attempts to center the player, and if it
    would go beyond certain bounds (set in the editor) prefers to stay
    within those bounds.

---
+++ Client/Assets/Scripts/CenterCameraOnPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterCameraOnPlayer : MonoBehaviour {
+
+	Transform pTransform = null;
+	public float leftBound = 0;
+	public float rightBound = 10 * 4;
+	public float topBound = 0;
+	public float bottomBound = -8 * 4;
+	float camHWidth;
+	float camHHeight;
+
+	void Start(){
+		Camera ccam = FindObjectOfType<Camera> ();
+		camHHeight = ccam.orthographicSize;
+		camHWidth = ccam.aspect * camHHeight;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		if (pTransform == null) {
+			GameObject target = GameObject.Find ("Player 1(Clone)");
+			if (target) {
+				pTransform = target.transform;
+				LateUpdate ();
+			}
+		} else {
+			float targetX = pTransform.position.x;
+			float targetY = pTransform.position.y;
+			if (targetX - camHWidth < leftBound) {
+				targetX = leftBound + camHWidth;
+			}
+			if (targetX + camHWidth > rightBound) {
+				targetX = rightBound - camHWidth;
+			}
+			if (targetY - camHHeight < bottomBound) {
+				targetY = bottomBound + camHHeight;
+			}
+			if (targetY + camHHeight > topBound) {
+				targetY = topBound - camHHeight;
+			}
+			transform.position = (Vector3.right * targetX) + (Vector3.up * targetY) + (Vector3.back * 10);
+		}
+	}
+}
